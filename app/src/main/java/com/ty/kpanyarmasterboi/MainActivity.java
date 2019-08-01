1b04576302c39b9bd5346a4e71a1db42343e32e1
Mavis Clouds <klogi99@gmail.com>
2019-08-02 06:37:00 +0700

    ws

--- app/src/main/java/com/ty/kpanyarmasterboi/MainActivity.java
+++ app/src/main/java/com/ty/kpanyarmasterboi/MainActivity.java
@@ -73,14 +73,14 @@ public class MainActivity extends AppCompatActivity
          */
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+       /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         /**
         *Drawer was here
@@ -135,12 +135,12 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+      /*  int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
